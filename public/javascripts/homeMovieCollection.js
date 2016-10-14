65bf015ae18c9e1f71ea8869f67ac7ab09d68a79
paulcarroll <pvcarroll@gmail.com>
2016-10-14 16:32:30 -0500

    react working

---
+++ public/javascripts/homeMovieCollection.js
@@ -0,0 +1,12 @@
+var MovieCollection = React.createClass({
+  render: function() {
+    return (
+        <div>MovieCollection</div>
+    );
+  }
+});
+
+ReactDOM.render(
+  <MovieCollection />,
+  document.getElementById("container")
+);
