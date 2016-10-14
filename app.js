991ba10967e485ad16ca9b3e1b1feb9ea9b13601
paulcarroll <pvcarroll@gmail.com>
2016-10-14 15:12:19 -0500

    replace views with public/index.html

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
+// app.set('view engine', 'html');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
@@ -49,11 +49,10 @@ if (app.get('env') === 'development') {
 // no stacktraces leaked to user
 app.use(function(err, req, res, next) {
   res.status(err.status || 500);
-  res.render('error', {
+  res.render('', {
     message: err.message,
     error: {}
   });
 });
-
 
 module.exports = app;
